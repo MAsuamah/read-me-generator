a5d1038318f62943df1a090e922d2f2710cebc26
Michelle <michey.asmah21@gmail.com>
2021-04-15 20:04:00 -0400

    questions array

--- index.js
+++ index.js
@@ -18,7 +18,67 @@ const questions = [
         }
       }
     },
-
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project: (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project description!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmInstall',
+      message: 'Would you like to add installation instructions?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'Provide installation instructions:',
+      when: ({ confirmInstall }) => confirmInstall
+    },
+    {
+      type: 'confirm',
+      name: 'confirmUsageInfo',
+      message: 'Would you like to add usage instructions?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage instructions:',
+      when: ({ confirmUsageInfo }) => confirmUsageInfo
+    },
+    {
+      type: 'confirm',
+      name: 'confirmContribution',
+      message: 'Would you like to add contribution guidelines?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide contribution guidelines:',
+      when: ({ confirmContribution }) => confirmContribution
+    },
+    {
+    type: 'confirm',
+    name: 'confirmTest',
+    message: 'Would you like to add test instructions?',
+    default: true
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide test instructions:',
+    when: ({ confirmTest }) => confirmTest
+  },
 ];
 
 // TODO: Create a function to write README file
